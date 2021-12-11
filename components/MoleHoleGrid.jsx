e30fa1292acc1162df8e93405e0eb1b52b322deb
andymcgunagle <amcgunagle@gmail.com>
2021-12-11 15:49:16 -0800

    Adjusted sizing for smaller screens.

--- components/MoleHoleGrid.jsx
+++ components/MoleHoleGrid.jsx
@@ -23,7 +23,7 @@ export default function MoleHoleGrid({ time, score, setScore }) {
 
 function MoleHole({ time, score, setScore }) {
   return (
-    <div className="flex justify-center items-center bg-gray-700 border-8 border-red-900 rounded-full h-12 w-12 md:p-16 p-10 text-4xl shadow-2xl">
+    <div className="flex justify-center items-center bg-gray-700 border-8 border-red-900 rounded-full h-12 w-12 p-14 md:p-16 text-4xl shadow-2xl">
       <Mole time={time} score={score} setScore={setScore} />
     </div>
   );
